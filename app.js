a4786eb38cff1354b28489243526b5860ba15cca
parth1947 <31795745+parth1947@users.noreply.github.com>
2020-07-05 06:06:29 +0530

    ji

--- app.js
+++ app.js
@@ -8,25 +8,25 @@ const app = express();
 app.use(cors());
 
 
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://parth123:parth123@cluster0.yjojy.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   console.log('Connect to Db! Hurray!!')
-//   client.close();
-// });
-// mongoose.connect('mongodb+srv://parth123:parth123@cluster0.yjojy.mongodb.net/Director?retryWrites=true&w=majority' , {
-// 	useNewUrlParser: true,
-// 	useCreateIndex: true,
-//   useUnifiedTopology: true
-// })
-// .then(()=> {
-// 	console.log("Connected to DB! Hurray");
-// }).catch(err => {
-// 	console.log("ERROR:", err.message);
-// })
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://parth123:parth123@cluster0.yjojy.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log('Connect to Db! Hurray!!')
+  client.close();
+});
+mongoose.connect('mongodb+srv://parth123:parth123@cluster0.yjojy.mongodb.net/Director?retryWrites=true&w=majority' , {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+  useUnifiedTopology: true
+})
+.then(()=> {
+	console.log("Connected to DB! Hurray");
+}).catch(err => {
+	console.log("ERROR:", err.message);
+})
 
 // mongoose.connect('mongodb+srv://parth123:parth123@cluster0.yjojy.mongodb.net/test?retryWrites=true&w=majority');
 // mongoose.connection.once('open', ()=>{
